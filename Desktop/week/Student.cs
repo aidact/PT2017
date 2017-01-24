6e61169ad7cb528e0108235ecf412abec449f610
aidact <aida.ct127@gmail.com>
2017-01-24 21:47:47 +0600

    2 codes on c#: class and prime numbers

---
+++ Desktop/week/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Student
+{
+    class Program
+    {
+        class Student //создаем новый класс
+        {
+            public string name; 
+            public string surname;
+            public double gpa;
+            public Student (string name, string surname)
+                 {
+                     this.name = name; // указываем на экзэмпляр
+                     this.surname = surname;
+                 }
+            public double GPA()
+            {
+                return gpa;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Student a = new Student("Lola", "Umarova"); // задаем все значения
+            a.gpa = 3.98;
+            Console.WriteLine(a.name +' '+ a.surname +' '+ a.gpa); // вывод на экран
+            Console.ReadKey();
+        }
+    }
+}
