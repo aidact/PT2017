4bba132fb2a758a2b2b432b27cc30cbb636f117f
aidact <aida.ct127@gmail.com>
2017-01-25 08:06:34 +0600

    ver. with files

---
+++ Desktop/New/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        public static void Sol()
+        {
+            StreamReader sr = new StreamReader(@"c:HW\a.txt");
+            StreamWriter sw = new StreamWriter(@"c:HW\b.txt");
+            string[] arr = sr.ReadLine().Split();
+                      
+                sw.WriteLine("max:" + arr.Max() + ' ' + "min:" + arr.Min());
+                sr.Close();
+                sw.Close();
+            
+        }
+        static void Main(string[] args)
+        {
+            Sol();
+            //Console.ReadKey();
+            
+        }
+    }
+}
