29d3010467b290851f6bef15ee13c7cbe10212b4
aidact <aida.ct127@gmail.com>
2017-02-09 11:40:21 +0600

    added far manager

---
+++ Desktop/MaxMin/ConsoleApplication4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        public static void Sol()
+        {
+            int max=0, min=100000;
+            StreamReader sr = new StreamReader(@"c:\Users\Aida\Desktop\track\a.txt");
+            StreamWriter sw = new StreamWriter(@"c:\Users\Aida\Desktop\track\b.txt");
+            string[] arr = sr.ReadLine().Split();
+
+            foreach (string q in arr)
+            {
+                int n = Math.Abs(q);
+
+                if (max <= int.Parse(q)) 
+                {
+                    max = int.Parse(q);
+                }
+
+                if (min >= int.Parse(q)) 
+                {
+                    min = int.Parse(q);
+                }
+            }
+            sw.WriteLine("Max:" + max + ' ' + "Min" + min);
+            sr.Close();
+            sw.Close();
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Sol();
+
+        }
+    }
+}
