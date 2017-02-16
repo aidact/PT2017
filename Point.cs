d6293ef95acccb951e0e74f72ffc58a6b0cce210
aidact <aida.ct127@gmail.com>
2017-02-16 11:04:28 +0600

    added game

---
+++ Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Point
+    {
+        public int x, y;
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
