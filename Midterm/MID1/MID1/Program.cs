4ae269f5674fe8479db9b0ad79c73ac7b08362ae
aidact <aida.ct127@gmail.com>
2017-03-09 12:16:52 +0600

    added file

---
+++ Midterm/MID1/MID1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MID1
+{
+    class Program
+    {
+        public static void check(int a)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.Write(i + " "); 
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            string str = Console.ReadLine();
+            int q = Convert.ToInt32(str);         
+            check(q);
+
+            Console.ReadKey();
+        }
+    }
+}
